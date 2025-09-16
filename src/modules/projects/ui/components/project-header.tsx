39b4d8a1271bffd74f8210c8c611f4bc75ca3ab6
rama <jihanlobs@gmail.com>
2025-09-16 18:15:17 +0700

    add postinstall prisma generate

--- src/modules/projects/ui/components/project-header.tsx
+++ src/modules/projects/ui/components/project-header.tsx
@@ -2,12 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import {
-  ChevronDownIcon,
-  ChevronLeftIcon,
-  EditIcon,
-  SunMoonIcon,
-} from "lucide-react";
+import { ChevronDownIcon, ChevronLeftIcon, SunMoonIcon } from "lucide-react";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import {
